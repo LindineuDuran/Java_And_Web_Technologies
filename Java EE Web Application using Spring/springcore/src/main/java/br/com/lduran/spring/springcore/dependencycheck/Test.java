2c91d0176450522dc046a83505bc2aa135aa41c5
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2025-05-30 16:51:09 -0300

    Dependency Check

---
+++ Java EE Web Application using Spring/springcore/src/main/java/br/com/lduran/spring/springcore/dependencycheck/Test.java
@@ -0,0 +1,17 @@
+package br.com.lduran.spring.springcore.dependencycheck;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"br/com/lduran/spring/springcore/dependencycheck/config.xml");
+		Prescription prescription = (Prescription) context.getBean("prescription");
+		System.out.println(prescription);
+	}
+
+}
